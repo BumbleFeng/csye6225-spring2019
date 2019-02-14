f36dd287393c7891cb821e441bcd21f183bb4b5d
Feng Huang <huang.fe@husky.neu.edu>
2019-02-14 16:58:50 -0500

    as3

--- webapp/src/main/java/com/me/webapi/repository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.me.webapi.repository;
-
-import com.me.webapi.pojo.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<Note, String> {
-
-}
